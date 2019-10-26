437aa3d9b16404a1a2f7fcd692f78e4dbc620949
JohanHallager <56110763+JohanHallager@users.noreply.github.com>
2019-10-26 21:52:48 +0200

    Add Car Plugins
    Add grid editor car
    Add WebAPI CarsApiController

---
+++ CodeCaseAutoIT/App_Plugins/Cars/editor/editor.controller.js
@@ -0,0 +1,69 @@
+﻿angular.module("umbraco").controller("cars.grideditorcontroller", function ($scope, $http, dialogService ) {
+
+    $scope.car = $scope.control.value;
+
+    $scope.openDialogClick = openDialog;
+
+    if ($scope.car === null) {
+        openDialog();
+    }
+
+
+    function openDialog() {
+
+            dialogService.open({
+                template: '/App_Plugins/Cars/editor/dialogview.html',
+                callback: function (data) {
+                    console.log("data back", data);
+                    $scope.car = data;
+                    $scope.control.value = data;
+                },
+                show: true
+            });
+    }
+
+
+});
+
+
+angular.module("umbraco").controller("cars.GetOriginsDialogcontroller", function ($scope, $http, dialogService) {
+    $scope.origins = null;
+    $scope.selectedItem = null;
+    $scope.cars = null;
+    $scope.selectedCar = null;
+    $scope.search = "";
+
+    $http({
+        method: 'GET',
+        url: '/umbraco/Api/CarsApi/GetOrigins'
+    }).then(function successCallback(response) {
+        console.log(response.data);
+        $scope.origins = response.data;
+    },
+    function errorCallback(response) {
+        console.log(response);
+    });
+
+    $scope.onSelectCar = function (car) {
+        $scope.selectedCar = car;
+    };
+
+    $scope.getAllCarsByOrigin = function () {
+
+        $http({
+            method: 'GET',
+            url: '/umbraco/Api/CarsApi/GetAllCarsByOrigin?origin=' + $scope.selectedItem
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            $scope.cars = response.data;
+
+
+        },
+            function errorCallback(response) {
+                console.log(response);
+            });
+    };
+
+
+});
+
---
+++ CodeCaseAutoIT/Controllers/WebAPI/CarsApiController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.WebApi;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using Newtonsoft.Json;
+
+namespace CodeCaseAutoIT.Controllers.WebAPI
+{
+    public class CarsApiController : UmbracoApiController
+    {
+        public List<Car> _cars { get; set; }
+        public CarsApiController()
+        {
+            var jsonDataFile = System.IO.File.ReadAllText(HostingEnvironment.MapPath(@"~/json_data/cars_data.json"));
+            var cars =  JsonConvert.DeserializeObject<List<Car>>(jsonDataFile);
+            if (cars.Any())
+            {
+                _cars = cars;
+            }
+
+        }
+
+        // /umbraco/Api/CarsApi/GetCars
+        [HttpGet]
+        public List<Car> GetCars()
+        {
+
+            return _cars;
+        }
+
+        // /umbraco/Api/CarsApi/GetCarByName?name=string
+        [HttpGet]
+        public Car GetCarByName(string name)
+        {
+            var car = _cars.FirstOrDefault(c => c.Name.ToLower() == name.ToLower());
+            return car;
+        }
+
+        
+        public List<Car> GetAllCarsByOrigin(string origin)
+        {
+            var cars = _cars.Where(c => c.Origin.ToLower() == origin.ToLower());
+            return cars.ToList();
+        }
+        // /umbraco/Api/CarsApi/GetOrigins
+        [HttpGet]
+        public List<string> GetOrigins()
+        {
+            var origins = _cars.GroupBy(c => c.Origin).Select(o=>o.Key);
+
+            return origins.ToList();
+        }
+    }
+
+    public class Car
+    {
+        public string Name { get; set; }
+        public float? Miles_per_Gallon { get; set; }
+        public int Cylinders { get; set; }
+        public float Displacement { get; set; }
+        public int? Horsepower { get; set; }
+        public int Weight_in_lbs { get; set; }
+        public float Acceleration { get; set; }
+        public string Year { get; set; }
+        public string Origin { get; set; }
+
+    }
+
+
+
+}
